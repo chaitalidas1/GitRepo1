ae44336acfa41c3b807fc6ab224f8a2a8801be84
Chaitali Das <chaitali.das1@gmail.com>
2025-09-21 22:26:38 -0700

    Add two new files

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Upload a File and then download a file and update")
+
+uploaded_file = st.file_uploader("Choose a file", type=["csv", "txt", "pdf", "png", "jpg"])
+if uploaded_file is not None:
+    st.success(f"Uploaded: {uploaded_file.name}")
+    # To save:
+    with open(uploaded_file.name, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.info("File saved locally.")
